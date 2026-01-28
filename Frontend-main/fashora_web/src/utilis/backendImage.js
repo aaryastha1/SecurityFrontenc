4b3507923212ad32c4c7544bbe0f2e1671d049ec
Your Name <your.email@example.com>
2026-01-28 14:22:48 +0545

    up

--- Frontend-main/fashora_web/src/utilis/backendImage.js
+++ Frontend-main/fashora_web/src/utilis/backendImage.js
@@ -1,11 +1,4 @@
-// export const getBackendImageUrl = (imagePath) => {
-//     if (!imagePath) return null
 
-//     const baseUrl = import.meta.env.VITE_BACKEND_URL ||
-//         "http://localhost:5006"
-//     return baseUrl + "/" + imagePath
-
-// }
 
 export const getBackendImageUrl = (imagePath, type = "product") => {
   if (!imagePath) return null;
