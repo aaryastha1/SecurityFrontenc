948d04d1ecb047cf2680e75e300d8f27c387b6d4
Your Name <your.email@example.com>
2026-01-29 12:28:56 +0545

    upd

--- Frontend-main/fashora_web/src/hooks/useLoginUser.js
+++ Frontend-main/fashora_web/src/hooks/useLoginUser.js
@@ -1,35 +1,4 @@
-// import { useMutation } from "@tanstack/react-query";
-// import { loginUserService } from "../services/authService";
-// import { toast } from "react-hot-toast";
-// import { useContext } from "react";
-// import { AuthContext } from "../auth/authProvider";
 
-// export const useLoginUser = () => {
-//   const { login } = useContext(AuthContext);
-
-//   return useMutation({
-//     mutationFn: loginUserService,
-//     mutationKey: ["login_key"],
-//     onSuccess: (data) => {
-//       if (!data) return;
-
-//       // If backend sends OTP message, do NOT login yet
-//       if (data.message?.includes("OTP sent")) {
-//         toast.success(data.message);
-//         return;
-//       }
-
-//       login(data?.data, data?.token);
-//       localStorage.setItem("userRole", data?.data?.role);
-//       localStorage.setItem("token", data?.token);
-//       localStorage.setItem("user", JSON.stringify(data?.data));
-//       toast.success(data?.message || "Login success");
-//     },
-//     onError: (err) => {
-//       toast.error(err?.response?.data?.message || err?.message || "Login failed");
-//     },
-//   });
-// };
 
 
 
