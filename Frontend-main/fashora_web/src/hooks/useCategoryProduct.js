a00b75d2d759ff4fc1d63533ac789cc9c9529e50
Your Name <your.email@example.com>
2026-01-29 12:27:39 +0545

    upd

--- Frontend-main/fashora_web/src/hooks/useCategoryProduct.js
+++ Frontend-main/fashora_web/src/hooks/useCategoryProduct.js
@@ -1,24 +1,4 @@
 
-
-// import { useQuery } from '@tanstack/react-query';
-// import axios from '../api/api';  // your axios instance baseURL should be set to http://localhost:5006/api
-
-// export const useProductsByCategory = (categoryName) => {
-//   return useQuery({
-//     queryKey: ['products', categoryName],
-//     queryFn: async () => {
-//       // Use query param route that works with your backend
-//       const res = await axios.get(`/user/products?categoryName=${categoryName}`);
-//       return res.data.products;  // array of products
-//     },
-//     enabled: !!categoryName,
-//   });
-// };
-
-
-
-
-// hooks/useCategoryProduct.js
 import { useQuery } from '@tanstack/react-query';
 import axios from '../api/api';
 import { useState } from 'react';
